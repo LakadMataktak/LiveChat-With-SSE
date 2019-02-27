91ea10d83ead09eafd24af03fc1fbbaa74728237
SubaruHasami <hmsbutterfly16@gmail.com>
2019-02-27 17:07:33 +0630

    'eh'

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
  	define(BASE_URL, "/");
  	$router = new routers();
  	$request_uri = strtolower($_SERVER["REQUEST_URI"]);
- 	echo $request_page = str_replace(strtolower(BASE_URL), "", $request_uri);
+ 	$request_page = str_replace(strtolower(BASE_URL), "", $request_uri);
 
 	if(!$request_page){$request_page = "home";}
-	echo $request_page;
-	exit();
 	require_once("routes.php");
 	$page = $router -> route_page($request_page);
 	if($page = $router -> route_page($request_page)) {
+		echo $page;
+		exit();
 		require_once($page);
 	}
 	
