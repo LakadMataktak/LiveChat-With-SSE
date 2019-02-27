3e2c20c15efccf57d55116348e1e7c5b7648080c
SubaruHasami <hmsbutterfly16@gmail.com>
2019-02-27 17:02:39 +0630

    beter

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  	require_once("vendor/autoload.php");
  	use tools\routers\routers as routers;
  	use tools\illuminate\illuminate as illuminate;
- 	define(BASE_URL, "/htoo_lab/live_chat/");
+ 	define(BASE_URL, "/");
  	$router = new routers();
  	$request_uri = strtolower($_SERVER["REQUEST_URI"]);
  	$request_page = str_replace(strtolower(BASE_URL), "", $request_uri);
